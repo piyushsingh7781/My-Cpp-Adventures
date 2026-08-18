ed72df8bf1710b09c4bfe39ae60a5c394938f9f6
piyushsingh7781 <piyushsingh7781@gmail.com>
2026-08-18 23:00:18 +0530

    Fixed Loops folder structure

---
+++ Loops/ASCII.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //for(int i=65;i<=90;i++){
+        //cout<<(char)i<<" ";
+    // }   for capital letters
+
+    //for(int i=65;i<=90;i++){
+        //cout<<(char)i<<" ";
+    //} for small letters
+
+    for(int i=65;i<=90;i++){
+        cout<<(char)i<<" "<<i<<endl;
+    }
+    //for printing both Letters and ASCII values
+}    
---
+++ Loops/GoodMorning.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    for(int i=1;i<=3;i+=1){
+        cout<<"Good Morning"<<endl;
+    }
+}
