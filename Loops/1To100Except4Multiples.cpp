e511f6c42b302868ac9fe44a3dd48991f8bfe4c3
piyushsingh7781 <piyushsingh7781@gmail.com>
2026-08-27 17:53:20 +0530

    Fix structure: keep only Loops folder

--- Loops/1To100Except4Multiples.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main (){
-    for (int i=1;i<=30;i++){
-        if (i%4 == 0) continue;
-        cout<<i<<" ";
-    }
-}
--- Loops/GoodMorning.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int n;
-    for(int i=1;i<=3;i+=1){
-        cout<<"Good Morning"<<endl;
-    }
-}
